e38552b8e23d73ddd6f7c63065971bfde431ff2c
gamaldarwish <darwishgamal802@gmail.com>
2026-04-26 00:59:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: [' I\'m Gamal Darwish', 'I\'m Front-End Developer','I\'m C++ Programmer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+};
+var typed = new Typed('.typing',options);
